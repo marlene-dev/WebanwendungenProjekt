1c0da0e0bca7cc37cf94c58d2ba96ba17598d066
Marlene Jansch <marlenejansch@gmail.com>
2023-08-03 00:51:58 +0200

    getelementbyid

---
+++ Frontend/userData/userData.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+var name = "Angela Ziegler";
+var email = "angela.ziegler@mail.com";
+var address = ["Bahnhofstrasse 23", "8001 Zürich"];
+var object = "help";
+
+document.getElementById('name').innerHTML = name;
+document.getElementById('email').innerHTML = email;
+document.getElementById('street').innerHTML = address[0];
+document.getElementById('town').innerHTML = adress[1];
+}
