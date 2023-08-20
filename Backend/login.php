5f213d51638c08233c4e749f6c5a7a5fce626282
Elina <edalewski@web.de>
2023-08-20 12:37:29 +0200

    small update for Status Code

--- Backend/login.php
+++ Backend/login.php
@@ -6,29 +6,31 @@ include 'connectToDatabase.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (isset($_POST["username"])) {
-    $input_username = $_POST['username'];
+    if (isset($_POST["email"])) {
+    $input_email = $_POST["email"];
     } else {
- //       echo "Please enter a username";
-    }
+        // Setze den HTTP-Statuscode 
+        http_response_code(400);  
+        echo "Zugriff verweigert. Die Email fehlt";    }
     if (isset($_POST["password"])) {
     $input_password = $_POST["password"];
     } else {
- //       echo "Please enter a password";
-    }
+        // Setze den HTTP-Statuscode 
+        http_response_code(400);  
+        echo "Zugriff verweigert. Das Passwort fehlt";    }
 
-/*
+
     try {
-        $sql = "SELECT * FROM userdata WHERE username = :username AND password = SHA2(:password, 256)";
+        $sql = "SELECT * FROM userdata WHERE email = :email AND password = SHA2(:password, 256)";
         $stmt = $conn->prepare($sql);
         $param = [
-            ':username' => $input_username,
+            ':email' => $input_email,
             ':password' => $input_password
         ];
         $stmt->execute($param);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        startSession($input_username);
+        startSession($result["id"]);
         //initialize testarray for JSON response
         $arrayData = ["username" => $input_username, "password" => $input_password];
         // do whatever we want with the users array.
@@ -38,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-*/
+
 
 if (isset($_POST["username"])) {
     $input_username = $_POST['username'];
@@ -49,6 +51,9 @@ if (isset($_POST["username"])) {
     if (isset($_POST["streetname"])) {
     $input_street = $_POST["streetname"];
     }
+    if (isset($_POST["number"])) {
+        $input_street = $_POST["number"];
+        }
     if (isset($_POST["plz"])) {
     $input_plz = $_POST["plz"];
     }
@@ -59,16 +64,17 @@ if (isset($_POST["username"])) {
     $input_country = $_POST["country"];
     }
 
-$arrayData = ["username" => "testName", 
+$arrayDataTest = ["username" => "testName", 
 "email" => "testEmail",
 "streetname" => "testStreet",
+"number" => "123",
 "plz" => "testPlz",
 "town" => "testTown",
 "country" => "testEngland"
 ];
 // do whatever we want with the users array.
 header('Content-type: application/json');
-echo json_encode( $arrayData );
+echo json_encode( $arrayDataTest );
 }
 
 $conn = null;
