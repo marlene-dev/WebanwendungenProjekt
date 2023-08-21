89f8168fed42e1503f4c285534d4261b64eea193
Marlene Jansch <marlenejansch@gmail.com>
2023-08-21 20:48:41 +0200

    elina change

--- changeData/changeData.js
+++ changeData/changeData.js
@@ -1,33 +1,40 @@
 document.getElementById("sendChange").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
+  const regexPattern = /^[a-zA-Z0-9\s-]+$/;
+  //funktion verlassen wenn keine gültige Postleitzahl
+  if (!regexPattern.test(document.getElementById("plz").value)) {
+    document.getElementById("badPlz").classList.remove("hidden");
+    return;
+  } else {
     // Create a new XMLHttpRequest instance
     const request = new XMLHttpRequest();
 
     const data = {
-    street: document.getElementById("street").value,
-    number: document.getElementById("number").value,
-    plz: document.getElementById("plz").value,
-    town: document.getElementById("town").value,
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      street: document.getElementById("street").value,
+      number: document.getElementById("number").value,
+      plz: document.getElementById("plz").value,
+      town: document.getElementById("town").value,
     };
     // Configure the request: Method and URL
     request.open("GET", "/Backend/saveInDatabase.php", true);
 
     // Set up a callback function to handle the response
-    request.onload = function() {
-        if (request.status === 200) {
-            console.log("Data changed");
-            window.location.replace("/userData/");
-        } else {
-            document.getElementById('errorMessage').classList.remove("hidden");
-        }
+    request.onload = function () {
+      if (request.status === 200) {
+        console.log("Daten wurden geändert");
+        window.location.replace("/userData/");
+      } else {
+        document.getElementById("badPlz").classList.add("hidden");
+        document.getElementById("errorMessage").classList.remove("hidden");
+      }
     };
-
     // Send the request
-    request.send(JSON.stringify(data));    
+    request.send(JSON.stringify(data));
+  }
 });
 
-function exitEdit(){
-
-window.location.replace("/changeData/");
-
+function exitEdit() {
+  window.location.replace("/changeData/");
 }
--- userData/userData.js
+++ userData/userData.js
@@ -1,51 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("logout").addEventListener("click", (e) => {
-        e.preventDefault();
-        // Create a new XMLHttpRequest instance
-        const request = new XMLHttpRequest();
+  document.getElementById("logout").addEventListener("click", (e) => {
+    e.preventDefault();
+    // Create a new XMLHttpRequest instance
+    const request = new XMLHttpRequest();
 
-        // Configure the request: Method and URL
-        request.open("GET", "/Backend/logout.php", true);
+    // Configure the request: Method and URL
+    request.open("GET", "/Backend/logout.php", true);
 
-        // Set up a callback function to handle the response
-        request.onload = function () {
-        if (request.status === 200) {
-            console.log("Logged out successfully");
-            // Redirect to a login page or another appropriate location
-            window.location.replace("/login/");
-        } else {
-            document.getElementById("errorMessage").classList.remove("hidden");
-        }
-        };
-
-        // Send the request
-        request.send();
-    });
-
-    // Funktion zum Abrufen und Anzeigen der Nutzerdaten
-    function fetchAndPopulateUserData() {
-        const request = new XMLHttpRequest();
-        request.open("GET", "/Backend/getUserData.php", true);
-        request.onload = function () {
-        if (request.status === 200) {
-            const responseData = JSON.parse(request.responseText);
-            document.getElementById("name").value = responseData.username;
-            document.getElementById("email").value = responseData.email;
-            document.getElementById("street").value = responseData.streetname;
-            document.getElementById("number").value = responseData.streetnumber;
-            document.getElementById("plz").value = responseData.plz;
-            document.getElementById("town").value = responseData.town;        
-        } else {
-            console.error("An error occurred while fetching user data.");
-            window.location.replace("/login/");
-        }
-        };
-        request.send();
-    }
-    //aufrufen der Funktion um Userdata zu laden
-    fetchAndPopulateUserData();
-    
-    function changeData() {
-        window.location.replace("/changeData/");
-    }
+    // Set up a callback function to handle the response
+    request.onload = function () {
+      if (request.status === 200) {
+        console.log("Logged out successfully");
+        // Redirect to a login page or another appropriate location
+        window.location.replace("/login/");
+      } else {
+        document.getElementById("errorMessage").classList.remove("hidden");
+      }
+    };
+    // Send the request
+    request.send();
+  });
 });
+// Funktion zum Abrufen und Anzeigen der Nutzerdaten
+window.onload = function (){
+  const request = new XMLHttpRequest();
+  request.open("GET", "/Backend/getUserData.php", true);
+  request.onload = function () {
+    if (request.status === 200) {
+      const responseData = JSON.parse(request.responseText);
+      document.getElementById("name").innerHTML = responseData.username;
+      document.getElementById("email").innerHTML = responseData.email;
+      document.getElementById("street").innerHTML = responseData.streetname;
+      document.getElementById("number").innerHTML = responseData.streetnumber;
+      document.getElementById("plz").innerHTML = responseData.plz;
+      document.getElementById("town").innerHTML = responseData.town;
+    } else {
+      alert("An error occurred while fetching user data.");
+      window.location.replace("/login/");
+    }
+  };
+  request.send();
+}
+
+function changeData() {
+  window.location.replace("/changeData/");
+}
