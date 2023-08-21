ad2dc99952756f6f5481754e7eb9cbab9926f243
Dennis Jacob <dennis.jacob128@gmail.com>
2023-08-21 15:41:29 +0200

    fixed form bug

--- login/login.js
+++ login/login.js
@@ -1,17 +1,6 @@
-function hide(hide) {
-  const hideElement = document.getElementById(hide);
-  // Change the HTML file displayed
-  hideElement.classList.add("hidden");
-}
-//zwingt Elemente auf Website die vorher verborgen waren
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-function show(show) {
-  const showElement = document.getElementById(show);
-  showElement.classList.remove("hidden");
-}
-
-function sendLogin() {
-  // Erstelle eine XMLHttpRequest-Instanz
   const request = new XMLHttpRequest();
   const data = {
     email: document.getElementById("email").value,
@@ -28,4 +17,16 @@ function sendLogin() {
     }
   };
   request.send(JSON.stringify(data));
+});
+
+function hide(hide) {
+  const hideElement = document.getElementById(hide);
+  // Change the HTML file displayed
+  hideElement.classList.add("hidden");
+}
+//zwingt Elemente auf Website die vorher verborgen waren
+
+function show(show) {
+  const showElement = document.getElementById(show);
+  showElement.classList.remove("hidden");
 }
